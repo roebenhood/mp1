45eeab1c6b1409a04101f9349dc19c1479a9db03
Roeben Pierce Boyles <roebenhood@gmail.com>
2023-01-13 21:13:54 -0800

    added functions in forms

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,38 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+var myModal = document.getElementById('myModal');
+var myInput = document.getElementById('myInput');
+var isCompleted = false;
+
+function clickedRadio(val){
+    let showCompletedDate = document.getElementById("divCompleted");
+    isCompleted = val;
+    isCompleted === true ? showCompletedDate.style.display = "block" : showCompletedDate.style.display = "none"
+    console.log(isCompleted);
+}
+
+function saveData(){
+    let goalName = document.getElementById("goalName").value;
+    let goalDescription = document.getElementById("goalDescription").value;
+    let targetDate = document.getElementById("targetDate").value;
+    let completedDate =  document.getElementById("completedDate").value;
+
+    console.log(goalName + '\n' + goalDescription + '\n' + targetDate + '\n' + isCompleted+ '\n' +completedDate);
+    
+}
+
+function disableField() {
+    const invalidForm = document.querySelector('form:invalid');
+    const submitBtn = document.getElementById('submit');
+    if (invalidForm) {
+      submitBtn.setAttribute('disabled', true);
+    } else {
+      submitBtn.disabled = false;
+    }
+  }
+
+
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+
